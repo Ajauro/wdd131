9e6d0ca361bc9088bd1024c074ba919bafaeddb0
Ajauro <sou24013@byui.edu>
2024-06-13 21:38:19 -0400

    added

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+// getdates.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Set the current year in the footer
+    const currentYear = new Date().getFullYear();
+    document.getElementById('currentYear').textContent = currentYear;
+
+    // Set the last modified date in the footer
+    const lastModified = document.lastModified;
+    document.getElementById('lastModified').textContent = `Last Modification: ${lastModified}`;
+});
