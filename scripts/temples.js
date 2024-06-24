aee7f9d0762ce5a9152ca94339d0cfacfacd41f2
Ajauro <sou24013@byui.edu>
2024-06-24 00:41:47 -0400

    added

--- scripts/temples.js
+++ scripts/temples.js
@@ -16,11 +16,7 @@ const hamburgerBtn = document.querySelector("#hamburger-btn");
 const closeMenuBtn = document.querySelector("#close-menu-btn");
 
 // Toggle mobile menu on hamburger button click
-hamburgerBtn.addEventListener("click", () => {
-    header.classList.toggle("show-mobile-menu");
-});
+hamburgerBtn.addEventListener("click", () => header.classList.toggle("show-mobile-menu"));
 
 // Close mobile menu on close button click
-closeMenuBtn.addEventListener("click", () => {
-    header.classList.remove("show-mobile-menu");
-});
+closeMenuBtn.addEventListener("click", () => header.classList.remove("show-mobile-menu"));
